1390b92a3e0765717f0d77c3b07a1075a9cdbbb2
mike <samkomisha@gmail.com>
2021-10-20 14:04:54 +0300

    add urls for views

---
+++ ecommerce_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .main.views import ProductList, ProductDetail, ShopList
+
+urlpatterns = [
+    path('products/<int:pk>/', ProductDetail.as_view(), name='detailcreate'),
+    path('products/', ProductList.as_view(), name='listcreate'),
+    path('shops/', ShopList.as_view())
+]
--- ecommerce_platform/urls.py
+++ ecommerce_platform/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from shopapp.views import index_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view)
+    path('', index_view),
+    path('api/', include('ecommerce_api.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
