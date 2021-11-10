736c1461706af7c2da56ea78afdf3e47afbcff73
mike <samkomisha@gmail.com>
2021-11-10 13:14:05 +0300

    Create custom user model, create custom user manager. Use email as auth

--- shop_api/admin.py
+++ shop_api/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Product, Shop, ProductCategory
+from django.contrib.auth.admin import UserAdmin
+from .models import Product, Shop, ProductCategory, ShopInfo, NewUser
+
 
 admin.site.register(Product)
 admin.site.register(Shop)
 admin.site.register(ProductCategory)
+admin.site.register(ShopInfo)
+
+
+class UserAdminConfig(UserAdmin):
+    ordering = ('-date_joined', )
+    list_display = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'first_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')
+            }
+         ),
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
---
+++ shop_api/managers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_user(self, email, user_name, first_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You should provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name,
+                          first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, user_name, first_name, password, **other_fields)
