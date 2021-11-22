5bde3eea1337735b9ab8da1bc8cf84382628a549
mike <samkomisha@gmail.com>
2021-11-22 13:28:08 +0300

    add update and delete methods to detailed endpoints

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from .views import ProductViewSet, ShopViewSet, ShopInfoViewSet
 
 urlpatterns = [
-    path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve'})),
     path('products/', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('shops/', ShopViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('shops/<int:pk>/', ShopViewSet.as_view({'get': 'retrieve'})),
     path('shop_info/', ShopInfoViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('shop_info/<int:pk>/', ShopInfoViewSet.as_view({'get': 'retrieve'})),
+    path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve', 'put': 'update',
+                                                       'patch': 'partial_update', 'delete': 'destroy'})),
+    path('shops/<int:pk>/', ShopViewSet.as_view({'get': 'retrieve', 'put': 'update',
+                                                        'patch': 'partial_update', 'delete': 'destroy'})),
+    path('shop_info/<int:pk>/', ShopInfoViewSet.as_view({'get': 'retrieve', 'put': 'update',
+                                                         'patch': 'partial_update', 'delete': 'destroy'})),
 ]
