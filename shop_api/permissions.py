62c3bcfe908f05626d189b66b1e5c60b7f2d8368
mike <samkomisha@gmail.com>
2021-11-22 13:26:55 +0300

    add custom permissions in order to have wright access to edit objects

---
+++ shop_api/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+
+class ShopInfoWritePermission(permissions.BasePermission):
+    message = 'Editing shop info is restricted to the owner only'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user
+
+
+class ShopWritePermission(permissions.BasePermission):
+    message = 'Editing shop  is restricted to the owner only'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.shop_info.owner == request.user
+
+
+class ProductWritePermission(permissions.BasePermission):
+    message = 'Editing products is restricted to the creator only'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.creator == request.user
