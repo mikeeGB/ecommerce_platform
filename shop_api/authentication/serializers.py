b6a4c33a9e6e37391590b375fed7892f646adb9b
mike <samkomisha@gmail.com>
2021-11-25 20:46:05 +0300

    fix identation -> create method now works normally

--- shop_api/authentication/serializers.py
+++ shop_api/authentication/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from shop_api.models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
@@ -8,10 +9,10 @@ class RegisterUserSerializer(serializers.ModelSerializer):
         fields = ('email', 'user_name', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            password = validated_data.pop('password', None)
-            instance = self.model.objects.create(**validated_data)
-            if password is not None:
-                instance.set_password(password)
-            instance.save()
-            return instance
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
