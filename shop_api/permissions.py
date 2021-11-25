f5391032bb9ecc0f55d621e9358fb901e64711db
mike <samkomisha@gmail.com>
2021-11-25 17:46:07 +0300

    add has_permissiom method to custom permission classes in order to give access only for authenticated users

--- shop_api/permissions.py
+++ shop_api/permissions.py
@@ -4,6 +4,9 @@ from rest_framework import permissions
 class ShopInfoWritePermission(permissions.BasePermission):
     message = 'Editing shop info is restricted to the owner only'
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -14,6 +17,9 @@ class ShopInfoWritePermission(permissions.BasePermission):
 class ShopWritePermission(permissions.BasePermission):
     message = 'Editing shop  is restricted to the owner only'
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -23,6 +29,9 @@ class ShopWritePermission(permissions.BasePermission):
 
 class ProductWritePermission(permissions.BasePermission):
     message = 'Editing products is restricted to the creator only'
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
