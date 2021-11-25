bb893e953b37a9ae192565aa4af0fe9d74af768b
mike <samkomisha@gmail.com>
2021-11-25 17:45:07 +0300

    add APIView for logout

--- shop_api/authentication/views.py
+++ shop_api/authentication/views.py
@@ -2,7 +2,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterUserSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CustomUserCreate(APIView):
@@ -15,3 +16,16 @@ class CustomUserCreate(APIView):
             if custom_user:
                 return Response(status=status.HTTP_201_CREATED)
         return Response(registration_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlacklistToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
