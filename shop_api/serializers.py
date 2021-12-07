d9685a919b0558b4c5d12f2ab04b54bd6db04599
mike <samkomisha@gmail.com>
2021-12-07 13:01:32 +0300

    add create method to create or update shop instance. Add ReadOnly shop_name field to ProductSerializer and ShopSerializer

--- shop_api/serializers.py
+++ shop_api/serializers.py
@@ -19,16 +19,33 @@ class ShopInfoSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.email')
+    shop_name = serializers.ReadOnlyField(source='shop_inf.shop_name')
 
     class Meta:
         model = Product
-        fields = ('id', 'creator', 'shop_inf', 'title', 'category', 'description', 'image', 'price', 'quantity', 'size', 'color')
+        fields = ('id', 'creator', 'shop_inf', 'shop_name', 'title', 'category',
+                  'description', 'image', 'price', 'quantity', 'size', 'color')
 
 
 class ShopSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+    shop_name = serializers.ReadOnlyField(source='shop_info.shop_name')
+
+    def create(self, validated_data):
+        print(validated_data)
+        products = validated_data.pop('products')
+        # update shop instance if there is object with same shop_info in db, otherwise - create new shop
+        shop, created = Shop.objects.update_or_create(
+            shop_info=validated_data['shop_info'],
+            defaults={'shop_info': validated_data['shop_info']}
+        )
+        print("Shop created" if created else "Shop updated")
+        # adding products to shop instance (m-to-m relation)
+        for product in products:
+            shop.products.add(product)
+
+        return shop
 
     class Meta:
         model = Shop
-        fields = ('id', 'shop_info', 'products')
-
+        fields = ('id', 'shop_info', 'shop_name', 'products')
