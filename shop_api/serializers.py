42c593e4400cc1065db6410e25ad5b7de8d2e7dd
mike <samkomisha@gmail.com>
2021-12-07 11:09:19 +0300

    use shop_info.shop_name instead of shop_info.id

--- shop_api/serializers.py
+++ shop_api/serializers.py
@@ -19,16 +19,40 @@ class ShopInfoSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.email')
+    shop_name = serializers.CharField(source='shop_inf.shop_name')
+
+    def create(self, validated_data):
+        shop_name = validated_data['shop_inf'].get('shop_name')
+        shop_info_instance = ShopInfo.objects.filter(shop_name=shop_name).first()
+        validated_data['shop_inf'] = shop_info_instance  # update validated_data in order to pass shop_info instance
+        return Product.objects.create(**validated_data)
 
     class Meta:
         model = Product
-        fields = ('id', 'creator', 'shop_inf', 'title', 'category', 'description', 'image', 'price', 'quantity', 'size', 'color')
+        fields = ('id', 'creator', 'shop_name', 'title', 'category',
+                  'description', 'image', 'price', 'quantity', 'size', 'color')
 
 
 class ShopSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+    shop_name = serializers.CharField(source='shop_info.shop_name')
+
+    def create(self, validated_data):
+        print(validated_data)
+        shop_name = validated_data['shop_info'].get('shop_name')
+        shop_info_instance = ShopInfo.objects.filter(shop_name=shop_name).first()
+        # validated_data['shop_info'] = shop_info_instance  # update validated_data in order to pass shop_info instance
+        products = validated_data.pop('products')
+
+        shops, created = Shop.objects.update_or_create(
+            shop_info__shop_name=validated_data['shop_info'].get('shop_name'),
+            defaults={'shop_info': shop_info_instance}
+        )
+        print(created)
+        for product in products:
+            shops.products.add(product)
+        return shops
 
     class Meta:
         model = Shop
-        fields = ('id', 'shop_info', 'products')
-
+        fields = ('id', 'shop_name', 'products')
--- shop_api/views.py
+++ shop_api/views.py
@@ -17,12 +17,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['$title']
 
+    def get_shop_names(self):
+        shop_infos = ShopInfo.objects.filter(owner=self.request.user)
+        shop_names = [shop_info.shop_name for shop_info in shop_infos]
+        print(shop_names)
+        return shop_names
+
     def perform_create(self, serializer):
-        if serializer.validated_data['shop_inf'].owner != self.request.user:
-            # check if chosen shop_inf owner is current user, if not - raise exception
+        print(serializer.validated_data)
+        if serializer.validated_data['shop_inf'].get('shop_name') not in self.get_shop_names():
             raise exceptions.PermissionDenied(
                 detail="You don't have a permission. Choose your shop"
             )
+
         serializer.save(creator=self.request.user)
 
 
@@ -36,16 +43,22 @@ class ShopViewSet(viewsets.ModelViewSet):
         shop = self.kwargs.get('pk')  # id from url
         return get_object_or_404(Shop, shop_info=shop)
 
+    def get_shop_names(self):
+        shop_infos = ShopInfo.objects.filter(owner=self.request.user)
+        shop_names = [shop_info.shop_name for shop_info in shop_infos]
+        print(shop_names)
+        return shop_names
+
     def perform_create(self, serializer):
-        shop_inf_id_from_req = int(self.request.data['shop_info'])
-        product_set = Product.objects.filter(shop_inf_id=shop_inf_id_from_req).all()
+        shop_name = self.request.data['shop_name']
+        product_set = Product.objects.filter(shop_inf__shop_name=shop_name).all()
+
         if not product_set:
             # if no products added to the shop - raise error
             raise exceptions.NotFound(
                 detail="Products not found. Please add products to your shop first"
             )
-        if serializer.validated_data['shop_info'].owner != self.request.user:
-            # check if chosen shop_info owner is current user, if not - raise exception
+        if serializer.validated_data['shop_info'].get('shop_name') not in self.get_shop_names():
             raise exceptions.PermissionDenied(
                 detail="You don't have a permission. Choose your shop"
             )
