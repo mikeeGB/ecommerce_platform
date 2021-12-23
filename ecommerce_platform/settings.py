9d82a8ae8f51bc037c09b03c330a7760e5934ec5
mike <samkomisha@gmail.com>
2021-12-23 17:17:28 +0300

    add configuration for using postgres image in docker. db is empty

--- ecommerce_platform/settings.py
+++ ecommerce_platform/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'ecommerce_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_ecommerce_db',
+        'USER': 'test_ecommerce',
+        'PASSWORD': 'test_ecommerce',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
 }
 
 
